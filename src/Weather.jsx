fb113584e4c02222418ea4222e44b9f4b30c270e
Dharshini <2112026@nec.edu.in>
2024-02-19 03:27:37 +0530

    first commit

---
+++ src/Weather.jsx
@@ -0,0 +1,54 @@
+import React, { useState} from "react";
+import './Weather.css';
+import Video from './assets/car.mp4';
+
+function Weather() {
+    const api = {
+        key: "3252fed708c47312fd9cac9c3dc35a38",
+        base: "https://api.openweathermap.org/data/2.5/",
+    };
+
+    const [weather, setWeather] = useState('');
+    const [data, setData] = useState({});
+
+    function Search() {
+        fetch(`${api.base}weather?q=${weather}&units=metric&APPID=${api.key}`)
+            .then((res) => res.json())
+            .then((res) => {
+                setData(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+
+    return (
+        <div>
+            <video autoPlay muted loop >
+                    <source src={Video}></source>
+            </video>
+            <div id="Main">
+                <h1>Weather App</h1>
+                <input onChange={(e) => setWeather(e.target.value)} placeholder="Enter city name"></input><br/>
+                <button onClick={Search}>Search</button>
+                {typeof data.main !== "undefined" ? (
+                    <div>
+                        <p>Location : {data.name}</p>
+                        <p>Celsius : {data.main.temp}°C</p>
+                        <p>Status : {data.weather?.[0]?.main}</p>
+                        <p>Atmospheric conditions : {data.weather?.[0]?.description}</p>
+                    </div>
+                ) : (
+                    <div>
+                        <p>Location :</p>
+                        <p>Celsius : </p>
+                        <p>Status : </p>
+                        <p>Atmospheric conditions :</p>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
